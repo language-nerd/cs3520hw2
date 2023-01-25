d20f1d352b9ac216c3a2a5e63fda1a29e89e7501
language-nerd <sebastian.toledo506@gmail.com>
2023-01-24 20:35:16 -0500

    started work on Matrix.cpp

---
+++ starter-files/Matrix.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "Matrix.hpp"
+
+// using namespace std; // Are we supposed to have this here?
+
+int main() {
+
+}
+
+int get_width() const {
+
+}
+
+int get_height() const {
+
+}
+
+int& at(int row, int column) {
+
+}
+
+const int& at(int row, int column) const {
+
+}
+
+Matrix::Slice get_row_slice(int row, int col_start, int col_end) {
+
+}
+
+void print(std::ostream& os) const {
+
+}
+
