b0ef287110e69f3eba2ea4179455ca755e67994b
marlaaaaine <98628128+marlaaaaine@users.noreply.github.com>
2023-01-24 22:43:39 -0500

    Update Matrix.cpp with all implementations
    Implemented all Matrix.hpp files in Matrix.cpp

--- starter-files/Matrix.cpp
+++ starter-files/Matrix.cpp
@@ -7,27 +7,39 @@ int main() {
 
 }
 
-int get_width() const {
-
+int Matrix::get_width() const {
+    return m_width;
 }
 
-int get_height() const {
-
+int Matrix::get_height() const {
+    return m_height;
 }
 
-int& at(int row, int column) {
-
+int& Matrix::at(int row, int column) {
+    return m_data.at(column + m_width * row); 
 }
 
-const int& at(int row, int column) const {
-
+const int& Matrix::at(int row, int column) const {
+    return m_data.at(column + m_width * row); 
 }
 
-Matrix::Slice get_row_slice(int row, int col_start, int col_end) {
-
+Matrix::Slice Matrix::get_row_slice(int row, int col_start, int col_end) {
+    std::vector<int> portion;
+    for(int i = col_start; i < col_end; ++i){
+        portion.push_back(this->at(row, i)); //is this allowed?
+    }
+    Slice row_slice = {portion, row, col_start, col_end};
+    return row_slice;
 }
 
-void print(std::ostream& os) const {
-
+void Matrix::print(std::ostream& os) const {
+ os << m_width << " " << m_height << "\n";
+ for(int row = 0; row < m_height; ++row) {
+    for(int col = 0; col < m_width; ++col) {
+        os << this->at(row, col) << " ";
+    }
+    os << "\n";
+ }
+ 
 }
 
