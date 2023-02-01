59d590cc3e9af02389ebc3f973ca2a5669aa4d8b
language-nerd <sebastian.toledo506@gmail.com>
2023-02-01 14:40:48 -0500

    compute_energy_matrix & compute_vertical_cost_matrix

--- starter-files/processing.cpp
+++ starter-files/processing.cpp
@@ -35,19 +35,51 @@ Image rotate_right(const Image& img) {
   return rotated_image;
 }
 Matrix compute_energy_matrix(const Image& img) {
-  Matrix energy = Matrix(img.get_width(), img.get_height());
+  Matrix energy_matrix = Matrix(img.get_width(), img.get_height());
+  int maximum_energy = 0;
 
-  for (int i = 0; i < img.get_width(); ++i){
-    for (int j = 0; j < img.get_height(); ++j){
-      int pixel_energy = 0;
-      //int pixel_energy = squared_difference(p1, Pixel p2) + squared_difference(Pixel p1, Pixel p2);
-      energy.at(i, j) = pixel_energy;
+  for (int i = 1; i < img.get_width()-1; ++i){
+    for (int j = 1; j < img.get_height()-1; ++j){
+      int position_energy = squared_difference(img.get_pixel(i, j-1), img.get_pixel(i, j+1)) + squared_difference(img.get_pixel(i-1, j), img.get_pixel(i+1, j));
+      if (position_energy > maximum_energy){
+        maximum_energy = position_energy;
+      }
+      energy_matrix.at(i, j) = position_energy;
     }
   }
 
+  for (int i = 0; i < img.get_width(); ++i){
+    for (int j = 0; j < img.get_height(); ++j){
+      if (i == 0 || j == 0 || i == img.get_width-1 || j == img.get_height-1){
+        energy_matrix.at(i, j) = maximum_energy;
+      }
+    }
+  }
 }
-Matrix compute_vertical_cost_matrix(const Image& img) {
 
+Matrix compute_vertical_cost_matrix(const Image& img) {
+  Matrix cost_matrix = compute_energy_matrix(img);
+
+  for (int j = 1; j < cost_matrix.get_height(); ++j){
+    for (int i = 0; i < cost_matrix.get_width(); ++i){
+      if (i == 0) { // if left edge
+        int cost = cost_matrix.at(i, j); // start by initializing cost to the energy of the current pixel
+        cost += min(cost_matrix.at(i, j-1), cost_matrix.at(i+1, j-1)); // add the min of the energy of the top and right
+        cost_matrix.at(i,j) = cost; // set the computed cost
+      }
+      else if (i == cost_matrix.get_width()-1) { // if right edge
+        int cost = cost_matrix.at(i, j); // start by initializing cost to the energy of the current pixel
+        cost += min(cost_matrix.at(i, j-1), cost_matrix.at(i-1, j-1)); // add the min of the energy of the top and left
+        cost_matrix.at(i,j) = cost; // set the computed cost
+      }
+      else {
+        int cost = cost_matrix.at(i, j); // start by initializing cost to the energy of the current pixel
+        cost += min(cost_matrix.at(cost_matrix.at(i-1, j-1), i, j-1), cost_matrix.at(i+1, j-1)); // add the min of the energy of the top, left, and right
+        cost_matrix.at(i,j) = cost; // set the computed cost
+      }
+    }
+  }
+  
 }
 std::vector<int> find_minimal_vertical_seam(const Matrix& cost) {
 
